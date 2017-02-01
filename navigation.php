d83617024625a235f00af44dc2a9ea49d64c261a
Evin Grost <evin.grost@gmail.com>
2017-02-01 16:08:10 -0600

    Trying to get php aspects working.

---
+++ navigation.php
@@ -0,0 +1,3 @@
+<?php 
+echo '<a href="index.html">Home</a> | <a href="BreedsPage.html">All About Breeds</a> | <a href="Dressage.html">Dressage</a> | <a href="ShowJumping.html">Show Jumping</a> | <a href="CrossCountry.html">Cross Country</a>';
+?>
